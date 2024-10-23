dfeb95038e7c97994333586ccc7acc03831dac7c
Willem van Heemstra <wvanheemstra@icloud.com>
2024-10-23 09:21:50 +0000

    Update flow

--- flask-app/flows/collaborative_pre_production_flow.py
+++ flask-app/flows/collaborative_pre_production_flow.py
@@ -1,5 +1,5 @@
 from prefect import flow, task
-from models import Director, Producer, Actor, Location, Storyboarder, Scene, CastingDirector, PrincipalCast, AboveTheLineCrew
+from models import Director, Producer, Actor, Location, Storyboarder, Scene, CastingDirector, PrincipalCast, AboveTheLineCrew, AssistantDirectorsDepartment
 
 @task
 def casting(casting_director: CastingDirector, principal_cast: PrincipalCast, actors: list):
@@ -8,8 +8,9 @@ def casting(casting_director: CastingDirector, principal_cast: PrincipalCast, ac
         principal_cast.actors.append(actor)  # Associate actor with principal cast
 
 @task
-def crew_assembly(director: Director, producer: Producer):
+def crew_assembly(director: Director, producer: Producer, ad_department: AssistantDirectorsDepartment):
     print(f"{director.name} and {producer.name} are assembling the crew.")
+    print(f"Assistant Directors Department includes: {', '.join([ad.name for ad in ad_department.first_assistant_directors])}, {', '.join([ad.name for ad in ad_department.second_assistant_directors])}, {', '.join([ad.name for ad in ad_department.second_second_assistants])}, {', '.join([ad.name for ad in ad_department.set_personal_assistants])}")
 
 @task
 def location_scouting(director: Director, producer: Producer, locations: list):
@@ -22,8 +23,8 @@ def storyboarding(storyboarder: Storyboarder, scenes: list):
         print(f"{storyboarder.name} is storyboarding scene: {scene.description}.")
 
 @flow
-def collaborative_pre_production_flow(director: Director, producer: Producer, casting_director: CastingDirector, principal_cast: PrincipalCast, above_the_line_crew: AboveTheLineCrew, actors: list, locations: list, storyboarder: Storyboarder, scenes: list):
+def collaborative_pre_production_flow(director: Director, producer: Producer, casting_director: CastingDirector, principal_cast: PrincipalCast, ad_department: AssistantDirectorsDepartment, actors: list, locations: list, storyboarder: Storyboarder, scenes: list):
     casting(casting_director, principal_cast, actors)
-    crew_assembly(director, producer)
+    crew_assembly(director, producer, ad_department)
     location_scouting(director, producer, locations)
     storyboarding(storyboarder, scenes)
